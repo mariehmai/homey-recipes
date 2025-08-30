d5fbebabc25bcffd0cda3bd99aa654e9079960a7
mariehmai <mai.mariehelene@gmail.com>
2025-08-30 18:13:46 +0100

    refactor: make fr default

--- app/i18n.ts
+++ app/i18n.ts
@@ -1,6 +1,6 @@
 export default {
   supportedLngs: ["en", "fr"],
-  fallbackLng: "en",
+  fallbackLng: "fr",
   // The default namespace of i18next is "translation", but you can customize it here
   defaultNS: "common",
 };
