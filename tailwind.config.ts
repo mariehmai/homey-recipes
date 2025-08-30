382df8113c058bb3fd2884cf4740588715b29378
mariehmai <mai.mariehelene@gmail.com>
2025-08-30 23:04:45 +0100

    refactor: remove unused bg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,6 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        home: "url('/assets/home-bg.jpg')",
-      },
-    },
+    extend: {},
   },
 } satisfies Config;
